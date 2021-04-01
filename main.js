b483d68e404a36392953a88c68eb499c68373bdb
Joshua Gossett <joshua.gossett@gmail.com>
2021-04-01 11:06:52 -0500

    Changes: - Create renderer web application. - Created application icons. - Created MainProcess.

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('Called main.js file.');
+const {app, BrowserWindow} = require('electron')
+
+let mainWindow;
+
+function createBrowserWindow() {
+    const browserWindow = new BrowserWindow({
+        title: 'Image Resize',
+        width: 500,
+        height: 600,
+        icon: `${__dirname}/assets/icons/icon__256x256.png`
+    });
+
+    browserWindow.loadFile(`./app/index.html`);
+
+    return browserWindow;
+}
+
+app.on('ready', () => {
+    mainWindow = createBrowserWindow();
+})
