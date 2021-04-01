c356977fbc6b0b3d9c2017f684b36bd169ed7c93
Joshua Gossett <joshua.gossett@gmail.com>
2021-04-01 17:08:41 -0500

    Created more logging.

--- tools/convert-icon-images.bat
+++ tools/convert-icon-images.bat
@@ -1,13 +1,14 @@
 @ECHO off
 
+SET FILE_NOT_FOUND_CODE=9009
+
 SET INKSCAPE_APPLICATION_PATH="%PROGRAMFILES%\Inkscape\bin\inkscape.com"
 
 SET FILE_NAME=icon__source.svg
 SET FOLDER_PATH=../assets/icons/
 SET FILE_PATH=%FOLDER_PATH%/%FILE_NAME%
 
-:: verify Inkscape is installed.
-
+:: Verify Inkscape is installed.
 IF not exist %INKSCAPE_APPLICATION_PATH% (
     ECHO.
     ECHO.ERROR: The `inkscape` command is not found. Run `choco install inkscape`.
@@ -15,32 +16,32 @@ IF not exist %INKSCAPE_APPLICATION_PATH% (
 )
 
 :: Verify Imagemagick is installed.
-magick -version
-IF NOT ERRORLEVEL 0 (
+magick -version > NUL
+IF %ERRORLEVEL%==%FILE_NOT_FOUND_CODE% (
      ECHO.
      ECHO.ERROR: The `imagemagick` command is not found. Run `choco install imagemagick`.
      EXIT /b 1
 )
 
-
 ::: Create PNG files.
-CALL:to-png-image %FILE_PATH% 16 16
-CALL:to-png-image %FILE_PATH% 32 32
-CALL:to-png-image %FILE_PATH% 48 48
-CALL:to-png-image %FILE_PATH% 64 64
-CALL:to-png-image %FILE_PATH% 128 128
-CALL:to-png-image %FILE_PATH% 256 256
+CALL:convert-to-png-image %FILE_PATH% 16 16
+CALL:convert-to-png-image %FILE_PATH% 32 32
+CALL:convert-to-png-image %FILE_PATH% 48 48
+CALL:convert-to-png-image %FILE_PATH% 64 64
+CALL:convert-to-png-image %FILE_PATH% 128 128
+CALL:convert-to-png-image %FILE_PATH% 256 256
 
 :: create ICO file.
 SET BASE_PNG_PATH=%FOLDER_PATH%icon__
 magick %BASE_PNG_PATH%16x16.png %BASE_PNG_PATH%32x32.png %BASE_PNG_PATH%48x48.png %BASE_PNG_PATH%256x256.png %FOLDER_PATH%icon.ico
+CALL:log "Created %FOLDER_PATH%icon.ico"
 
 EXIT /B %ERRORLEVEL%
 
 ::
-:: Resizes and converts a file into PNG
+:: Resizes and converts a SVG file into PNG file
 ::
-:to-png-image
+:convert-to-png-image
 SETLOCAL
 
 SET export_file_path=%1
